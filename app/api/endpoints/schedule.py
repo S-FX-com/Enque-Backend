614cb6319aaae5ab80f0349b6aa4436b62780cc9
Marrior-dot <mrlimaroberto@gmail.com>
2025-08-06 18:59:26 -0300

    schedule feature completed

--- app/api/endpoints/schedule.py
+++ app/api/endpoints/schedule.py
@@ -11,18 +11,27 @@ from app.models.agent import Agent as AgentModel
 from app.api.dependencies import get_current_active_user
 from app.api.endpoints.comments import create_comment
 from apscheduler.schedulers.blocking import BlockingScheduler
+from typing import Any
 
-router = APIRouter
+router = APIRouter()
 
-@router.post('/tasks/{task_id}/comments/schedule')
-async def create_scheduled_comment(task_id: int,
-comment_in: CommentCreate,
-request: Request,
-background_tasks: BackgroundTasks,
-date: datetime,
-db: Session = Depends(get_db),
-current_user: AgentModel = Depends(get_current_active_user),
+@router.post("/tasks/{task_id}/comments/schedule")  # <-- Path is the first argument
+async def create_scheduled_comment(
+    task_id: int,
+    comment_in: CommentCreate,
+    request: Request,
+    background_tasks: BackgroundTasks,
+    date: datetime,
+    db: Session = Depends(get_db),
+    current_user: AgentModel = Depends(get_current_active_user),
 ) -> Any:
     scheduled_task = BlockingScheduler()
-    scheduled_task.add_job(create_comment(task_id, comment_in, request, background_tasks, db, current_user), 'cron', day=date.day, month = date.month, year=date.year, hour=date.hour, minute=date.minute)
-    return
+    scheduled_task.add_job(
+        create_comment(task_id, comment_in, request, background_tasks, db, current_user),
+        'cron',
+        day=date.day,
+        month=date.month,
+        year=date.year,
+        hour=date.hour,
+        minute=date.minute
+    )
